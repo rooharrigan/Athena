2e84904d55eeab190a24dc34706ce23f841d5ef3
Roo Harrigan <harrigan.roo@gmail.com>
2015-10-27 12:24:02 -0700

    wikipedia API query now returns pythonized dictionary at infobox level

--- server.py
+++ server.py
@@ -21,43 +21,36 @@ query_params = {
     "formatverson": 2
     }
 
-def get_country(country):
+def get_country_infobox(country):
     """ API call to wikipedia to grab the string of JSON for the requested country.
-    Uses requests to turn the string into a python dictionary.
+    Uses requests to turn the string into a python dictionary and return a
+    dictionary that begins at the key 'Infobox country'
 
     """ 
+    #Query the wikipedia API for the JSON object, convert to Python dictionary
     query_params["titles"] = country
     r = requests.get("https://en.wikipedia.org/w/api.php?", params=query_params)
     info = r.json()
 
-    print "Here is the infobox:"
+    #Key into the dictionary to the Infobox level
     infobox_json = str(info["query"]["pages"].values()[0]["revisions"][0])
-    
 
-
-    # for i in infobox_json:
-    #     if i == "Infobox":
-    #         infobox_json[i:]
-    # else:
-    #     print "couldn't find the infobox"
+    #Parse the dictionary and filter it by wiki template names
     wikicode = mwparserfromhell.parse(infobox_json)
     templates = wikicode.filter_templates()
+
+    #Find the template with the string "infobox" in the name and return new dictionary
+    #starting at the Infobox level, parsed again
     j = 0
     for i in templates:
-        template = templates[j].name
-        tempalte = template.strip()
-        template = template.split()
-        print template
-        if template[0] == "Infobox":
-            print "Found Infobox!"
-        else:
-            print "No infobox yet"
+        country_dict = templates[j]
+        template = country_dict.name
+        template = template.strip()
+        if template.find("Infobox") != -1:
+            print "Got the infobox"
+            country_dict = mwparserfromhell.parse(country_dict)
+            pprint(country_dict[1900:3000])
         j += 1
-
-    #     # if template.name.matches("Infobox country"):
-    #     #     print "Yes!"
-    # else:
-    #     print "Still no infobox"
 
 
 
@@ -73,7 +66,7 @@ def parse_for_capital(info_file):
 
 
 
-get_country('kenya')
+get_country_infobox('kenya')
 
 
 
