a6b530452b373bbfecd680b875bcc3c876f66136
Roo Harrigan <harrigan.roo@gmail.com>
2015-10-27 15:32:10 -0700

    created homepage routethat collects country, quiz route that displays quiz question

--- server.py
+++ server.py
@@ -7,28 +7,52 @@ import requests, wikipedia, mwparserfromhell
 
 
 app = Flask(__name__)
+app.secret_key = "Get up, get up, get up, it's the first of the month."
 
 #Imports and flask app call
+
 ##############################################################################
+#Static Functions
 
-countries = ['kenya', 'somalia', 'nigeria', 'ethiopia']
+def get_capital(infobox_template, country):
+    """ Isolate the infobox template, grab the capital parameter out, and return it"""
+    capital = str(infobox_template.get("capital").value)
+    capital = capital[:-2]                                  #come back to this, it's icky
+    capital = translate(capital, None, '[]')    
 
-query_params = {
+    #TODO: Figure out a less brittle way to strip the \n character out.  
+    #\n is not being treated as a newline, but rather 2 separate characters
+    print country + ": " + capital
+    return capital
+
+##############################################################################
+#App Routes
+
+@app.route('/')
+def get_quiz_questions():
+    return render_template("quiz_country.html")
+
+
+@app.route('/quiz')
+def get_country_infobox():
+    """ API call to wikipedia to grab the string of JSON for the requested country
+    and return a dictionary that begins at the template/key 'Infobox country'
+
+    """ 
+    #Get country from the form:
+    country = request.args.get("country")
+
+    #Define query params
+    query_params = {
     "action": "query",
-    "titles": None,
+    "titles": country,
     "prop": "revisions",
     "rvprop": "content",
     "format": "json",
     "formatverson": 2
     }
 
-def get_country_infobox(country):
-    """ API call to wikipedia to grab the string of JSON for the requested country
-    and return a dictionary that begins at the template/key 'Infobox country'
-
-    """ 
     #Query the wikipedia API for the JSON object, convert to Python dictionary
-    query_params["titles"] = country
     r = requests.get("https://en.wikipedia.org/w/api.php?", params=query_params)
     info = r.json()
 
@@ -50,25 +74,20 @@ def get_country_infobox(country):
             print "Got the infobox!\n"
             template_list = mwparserfromhell.parse(country_dict).filter_templates()
             infobox_template = template_list[0]
-            parse_for_capital(infobox_template)
+            capital = get_capital(infobox_template, country)
+            return render_template("quiz_questions.html", country=country)
         j += 1
-        
 
-def parse_for_capital(infobox_template):
-    """ Isolate the infobox template, grab the capital parameter out, and return it"""
-    capital = str(infobox_template.get("capital").value)
-    capital = capital[:-2]                                  #come back to this, it's icky
-    capital = translate(capital, None, '[]')    
 
-    #TODO: Figure out a less brittle way to strip the \n character out.  
-    #\n is not being treated as a newline, but rather 2 separate characters
-    print capital
+
+
+
 
    
   
 
 
-get_country_infobox('kenya')
+# get_country_infobox('kenya')
 # get_country_infobox('nigeria')
 # get_country_infobox('canada')
 # get_country_infobox('england')
@@ -91,5 +110,8 @@ get_country_infobox('kenya')
 #Helper Functions
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    pass
+    app.run(debug=True)
+    
+
+
+
